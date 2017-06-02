96e8846f4fddbef15c9850a612c8340f3cab64c3
Rediet Ayalew <rediet.d.ayalew@gmail.com>
2017-06-02 15:38:49 -0400

    user input added

--- src/FenceCost.java
+++ src/FenceCost.java
@@ -1,23 +1,32 @@
+import java.util.Scanner;
 
 public class FenceCost {
 
 	public static void main(String[] args) {
 		
+		Scanner scn = new Scanner(System.in);
 		int woodFence = 25;
 		int chainLinkFence = 15;
 		int gate = 150;
 		int buildingPermit = 50;
 		int tax = 6;
-		boolean fenceWood= true;
-	    int fenceCost;
-		
-		int perimeter = 40;
-		int noOfDoors = 1;
+		int fenceCost;
+	    
+		System.out.println("Enter length ");
+		int length = scn.nextInt();
+		System.out.println("Enter length ");
+		int width = scn.nextInt();
+		System.out.println("Enter number of doors");
+		int noOfDoors = scn.nextInt();
+		scn.nextLine();
+		System.out.println("Choose the type of fence you want. Is it wood fence or chain link fence ? ");
+		String fenceType = scn.nextLine();
+		int perimeter = length * width;
 		int gateCost;
 		int salesCost;
 		int totalCost;
 		
-		if(fenceWood){
+		if(fenceType.equalsIgnoreCase("wood fence")){
 			fenceCost = perimeter * woodFence;
 		}else{
 			fenceCost = perimeter * chainLinkFence;
